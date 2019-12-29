5035be9d82b752050b05f857cac88439bca1bcc2
wangxiaoyuan007 <494006277@qq.com>
2019-12-29 12:09:19 +0800

    add:init project

---
+++ src/main/java/com/wxy/grpctest/GrpcTestApplication.java
@@ -0,0 +1,13 @@
+package com.wxy.grpctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wxy/grpctest/GrpcTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxy.grpctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
